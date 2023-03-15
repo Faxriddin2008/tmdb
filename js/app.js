20ef580796bd3f2055d821a60addd6649a966162
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-03-15 09:47:36 +0500

    tmdb full version

---
+++ js/app.js
@@ -0,0 +1,175 @@
+const slide = document.getElementById('slide')
+const slidee = document.getElementById('slidee')
+const slideee = document.getElementById('slideee')
+const trailer_list = document.getElementById("trailer-list")
+const movie_list = document.getElementById("movie-list")
+const tv_list = document.getElementById("tv-list")
+const apiKey = "18527347a9a13a6ec4a8563570d0507c";
+const imgUrl = "https://image.tmdb.org/t/p/original"
+const mainUrl = "https://api.themoviedb.org/3";
+const movieUrl = "https://www.themoviedb.org/"
+const movie = document.getElementById("movie");
+const pp = document.getElementById("pp");
+// import {
+//     slide,
+//     slidee,
+//     slideee,
+//     trailer_list,
+//     movie_list,
+//     tv_list,
+//     apiKey,
+//     imgUrl,
+//     mainUrl
+// } from "./urls.js";
+
+
+async function fetchApi(url){
+  await fetch(mainUrl+url+"?api_key="+apiKey)
+  .then(result => result.json())
+  .then(object =>  display(object.results))
+
+}
+function display(array){
+  // movie_list.innerHTML = "";
+  for (let i = 0;  i < array.length; i++){
+    const user = array[i];
+    console.log(user);
+    const {poster_path, backdrop_path, id, media_type, first_air_date, release_date, title, vote_average, name} = user;
+    card(user);
+  }
+  
+}
+
+
+
+function card(user){
+  movie_list.innerHTML +=
+  `
+    <div class="movie" id="movie" onclick="details(\`${user.id}\`,\`${user.media_type}\`, \`${user.title}\`, \`${user.name}\`)">
+            <img class="movie_photo" id="movie_photo" src="${imgUrl+user.poster_path}" alt="">
+            <div class="vote">${Math.floor(user.vote_average*10)}%</div>
+            <p class="movie_name">${user.name ?? user.title}</p>
+            <p class="movie_name release_date">${user.first_air_date ?? user.release_date}</p>
+    </div>
+    `;
+}
+async function fetchApii(url){
+  await fetch(mainUrl+url+"?api_key="+apiKey)
+  .then(result => result.json())
+  .then(object =>  displayy(object.results))
+
+}
+function displayy(array){
+  // movie_list.innerHTML = "";
+  for (let i = 0;  i < array.length; i++){
+    const user = array[i];
+    console.log(user);
+    const {poster_path, backdrop_path, id, media_type, first_air_date, release_date, title, vote_average, name} = user;
+    cardd(user);
+  }
+  
+}
+
+
+
+function cardd(user){
+  tv_list.innerHTML +=
+  `
+    <div class="movie" id="movie" onclick="details(\`${user.id}\`,\`${user.media_type}\`, \`${user.title}\`, \`${user.name}\`)">
+            <img class="movie_photo" id="movie_photo" src="${imgUrl+user.poster_path}" alt="">
+            <div class="vote">${Math.floor(user.vote_average*10)}%</div>
+            <p class="movie_name">${user.name ?? user.title}</p>
+            <p class="movie_name release_date">${user.first_air_date ?? user.release_date}</p>
+    </div>
+    `;
+}  
+
+
+async function fetchApiii(url){
+  await fetch(mainUrl+url+"?api_key="+apiKey)
+  .then(result => result.json())
+  .then(object =>  displayyy(object.results))
+
+}
+function displayyy(array){
+  // movie_list.innerHTML = "";
+  for (let i = 0;  i < array.length; i++){
+    const user = array[i];
+    console.log(user);
+    const {poster_path, backdrop_path, id, media_type, first_air_date, release_date, title, vote_average, name} = user;
+    carddd(user);
+  }
+  
+}
+
+
+
+function carddd(user){
+  trailer_list.innerHTML +=
+  `
+      <div class="trailer" onclick="details(\`${user.id}\`,\`${user.media_type}\`, \`${user.title}\`, \`${user.name}\`)">
+        <img src="${imgUrl+user.poster_path}" class="trailer-photo" alt="">
+        
+        <h2 class="trailer-name">${user.name ?? user.title}</h2>
+      </div>
+  `;
+}  
+
+
+function details(id, media_type , title, name){
+  location.replace(`${location.protocol}${location.host}other.html?id=${id}&media_type=${media_type}`);
+  // console.log(id);
+}
+
+
+
+  
+  fetchApi("/trending/all/day");
+  fetchApii("/tv/on_the_air")
+  fetchApiii("/movie/popular")
+
+
+
+  function week(){
+    movie_list.innerHTML = "";
+    fetchApi("/trending/all/week");
+    slide.style.marginLeft = "-100px";
+    slide.style.transition = "1s";
+  
+  }
+  function today(){
+    movie_list.innerHTML = "";
+    fetchApi("/trending/all/day");
+    slide.style.marginLeft = "-200px";
+    slide.style.transition = "1s";  
+  }
+  
+  function theatres(){
+    tv_list.innerHTML = "";
+    fetchApii("/tv/popular");
+    slidee.style.marginLeft = "-100px";
+    slidee.style.transition = "1s";  
+  }
+  function tv(){
+    tv_list.innerHTML = "";
+    fetchApii("/tv/on_the_air");
+    slidee.style.marginLeft = "-200px";
+    slidee.style.transition = "1s";  
+  }
+  
+  function theatres2(){
+    trailer_list.innerHTML = "";
+    fetchApiii("/tv/popular");
+    slideee.style.marginLeft = "-100px";
+    slideee.style.transition = "1s";  
+  }
+  function tv2(){
+    trailer_list.innerHTML = "";
+    fetchApiii("/movie/popular");
+    slideee.style.marginLeft = "-200px";
+    slideee.style.transition = "1s";  
+  }
+
+
+
+
---
+++ js/other.js
@@ -0,0 +1,111 @@
+const media_type = location.href.slice(location.href.search("media_type") + 11, location.length);
+const id = location.href.slice(location.href.search("id") + 3, location.length)
+console.log(id);
+console.log(media_type);
+
+const movieUrl = "https://www.themoviedb.org/"
+const mainUrl = "https://api.themoviedb.org/3";
+const apiKey = "18527347a9a13a6ec4a8563570d0507c";
+const imgUrl = "https://image.tmdb.org/t/p/original"
+const movieDetails = document.getElementById("movieDetails");
+const actors = document.getElementById('actors');
+async function fetchMovie(){
+    await fetch(`${mainUrl}/${media_type}/${id}?api_key=${apiKey}`)
+    .then(result => result.json())
+    .then(object => display(object))
+    // .then(object => console.log(object))
+}
+async function fetchActors(){
+    await fetch(`${mainUrl}/${media_type}/${id}/credits?api_key=${apiKey}`)
+    .then(result => result.json())
+    .then(object => displayy(object.cast))
+}
+function display(object){
+    console.log(object);
+    console.log(object.genres[0].name);
+    movieDetails.style.background = `url(${imgUrl}${object.backdrop_path ?? object.poster_path})`;
+    movieDetails.style.backgroundSize = `cover`;
+    movieDetails.style.backgroundPosition = `center`;
+    movieDetails.innerHTML = 
+    `
+    <div>
+            <img src="${imgUrl}${object.poster_path}" class="movie-photo" alt="">
+    </div>
+    <div class="details">
+        <h1 class="movie-name">${object.title ?? object.name}</h1>
+        <h5 class="release_date">${object.release_date ?? object.first_air_date} • ${object.genres[0].name}</h5>
+        <div class="detailss">
+            <div class="vote">
+                ${Math.round(object.vote_average * 10)} <sup class="sup">%</sup>
+            </div>
+            <h3 class="score">User <br> Score</h3>
+            <div class="list">
+                <div class="item"><i class='bx bx-menu'></i></div>               
+                <div class="item"><i class='bx bxs-heart'></i></div>               
+                <div class="item"><i class='bx bxs-bookmark'></i></div>               
+                <div class="item"><i class='bx bxs-star'></i></div>               
+            </div>
+        </div>
+        <p class="tagline">${object.tagline}</p>
+        <h1 class="headline">Overview</h1>
+        <p class="overview">${object.overview}</p>
+    </div>
+    `
+}
+function displayy(array){
+    console.log(array);
+    for (let i = 0; i < array.length; i++) {
+    if(array[i].profile_path){
+        actors.innerHTML += 
+        `
+        <div id="actor">
+                <img id="actor_photo" src="${imgUrl}${array[i].profile_path}" alt="">
+                <p id="actor_name">${array[i].name}</p>
+                <p id="character_name">${array[i].character}</p>
+    
+        </div>
+        `
+    }
+    
+    else if (array[i].gender == 0){
+        actors.innerHTML += 
+        `
+        <div id="actor">
+                <img id="actor_photo" src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-36-user-female-grey-d9222f16ec16a33ed5e2c9bbdca07a4c48db14008bbebbabced8f8ed1fa2ad59.svg" alt="">
+                <p id="actor_name">${array[i].name}</p>
+                <p id="character_name">${array[i].character}</p>
+    
+        </div>
+        `
+    }
+    else if (array[i].gender == 2){
+        actors.innerHTML += 
+        `
+        <div id="actor">
+                <img id="actor_photo" src="https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-4-user-grey-d8fe957375e70239d6abdd549fd7568c89281b2179b5f4470e2e12895792dfa5.svg" alt="">
+                <p id="actor_name">${array[i].name}</p>
+                <p id="character_name">${array[i].character}</p>
+    
+        </div>
+        `
+    }
+    }
+}
+
+
+
+
+
+
+
+
+
+fetchMovie()
+fetchActors()
+
+
+
+
+
+
+
